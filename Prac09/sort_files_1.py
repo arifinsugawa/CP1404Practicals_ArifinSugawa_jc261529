e7db469792bffcc6fbcc97d2ee456f6b36e3548a
Arifin <arifin.sugawa@my.jcu.edu.au>
2020-05-22 01:06:23 +0800

    Practical 9

---
+++ Prac09/sort_files_1.py
@@ -0,0 +1,18 @@
+import os
+
+
+def main():
+    for filename in os.listdir('.'):
+        if os.path.isdir(filename):
+            continue
+
+        extension = filename.split('.')[-1]
+        try:
+            os.mkdir(extension)
+        except FileExistsError:
+            pass
+        print("{}/{}".format(extension, filename))
+        os.rename(filename, "{}/{}".format(extension, filename))
+
+
+main()
